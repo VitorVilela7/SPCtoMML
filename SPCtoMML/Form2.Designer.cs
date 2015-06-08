3d6823527a492721e896872b4c3a1cc2f96324f0
Vitor Vilela <vitor.vilela.mario@gmail.com>
2015-06-07 21:28:10 -0300

    Progress/status bar form

---
+++ SPCtoMML/Form2.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SPCtoMML
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 28);
+			this.progressBar1.Maximum = 1000;
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(332, 23);
+			this.progressBar1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Margin = new System.Windows.Forms.Padding(3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(332, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "-";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(356, 59);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form2";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
---
+++ SPCtoMML/Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SPCtoMML
+{
+	public partial class Form2 : Form
+	{
+		/// <summary>
+		/// Callback for updating progress
+		/// </summary>
+		private Func<double> updateProgressHandler;
+
+		/// <summary>
+		/// Initializes the form.
+		/// </summary>
+		/// <param name="updateProgress">The callback for updating progress bar ratio.</param>
+		public Form2(Func<double> updateProgress)
+		{
+			InitializeComponent();
+			this.Text = Program.GetProgramName();
+			this.updateProgressHandler = updateProgress;
+		}
+
+		public void UpdateHandler(Func<double> updateProgress)
+		{
+			this.updateProgressHandler = updateProgress;
+		}
+
+		/// <summary>
+		/// Updates the progress dialog status.
+		/// </summary>
+		/// <param name="status">The new status to use.</param>
+		public void UpdateStatus(string status)
+		{
+			this.label1.Text = status;
+		}
+
+		/// <summary>
+		/// Updates the progress bar.
+		/// </summary>
+		/// <param name="ratio">The progress ratio.</param>
+		public void UpdateProgress(double ratio)
+		{
+			this.progressBar1.Value = (int)Math.Round(ratio * 1000);
+		}
+
+		private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (e.CloseReason == CloseReason.UserClosing)
+			{
+				e.Cancel = true;
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (updateProgressHandler != null)
+			{
+				UpdateProgress(updateProgressHandler());
+			}
+		}
+	}
+}
